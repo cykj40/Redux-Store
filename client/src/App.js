a0e199765e7265b70fabd30868a61b682492b848
cykj40 <102045473+cykj40@users.noreply.github.com>
2023-03-04 16:35:26 -0500

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import {
   ApolloClient,
   InMemoryCache,
@@ -14,9 +14,12 @@ import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Nav from './components/Nav';
+
 import { StoreProvider } from './utils/GlobalState';
 import Success from './pages/Success';
 import OrderHistory from './pages/OrderHistory';
+
+
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -44,37 +47,16 @@ function App() {
         <div>
           <StoreProvider>
             <Nav />
-            <Routes>
-              <Route 
-                path="/" 
-                element={<Home />} 
-              />
-              <Route 
-                path="/login" 
-                element={<Login />} 
-              />
-              <Route 
-                path="/signup" 
-                element={<Signup />} 
-              />
-              <Route 
-                path="/success" 
-                element={<Success />} 
-              />
-              <Route 
-                path="/orderHistory" 
-                element={<OrderHistory />} 
-              />
-              <Route 
-                path="/products/:id" 
-                element={<Detail />} 
-              />
-              <Route 
-                path="*" 
-                element={<NoMatch />} 
-              />
-            </Routes>
-          </StoreProvider>
+            <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route exact path="/login" component={Login}/>
+              <Route exact path="/signup" component={Signup}/> 
+              <Route exact path="/success" component={Success}/>
+              <Route exact path="/orderHistory" component={OrderHistory}/> 
+              <Route exact path="/products/:id" component={Detail}/> 
+              <Route component={NoMatch}/> 
+            </Switch>
+            </StoreProvider>
         </div>
       </Router>
     </ApolloProvider>
